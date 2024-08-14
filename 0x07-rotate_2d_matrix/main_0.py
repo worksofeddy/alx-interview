a71a778c9d6ce74e11f6413d2066f60dce128e34
worksofeddy <worksofeddy@gmail.com>
2024-08-14 14:00:47 +0300

    rotate

---
+++ 0x07-rotate_2d_matrix/main_0.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Test 0x07 - Rotate 2D Matrix
+"""
+rotate_2d_matrix = __import__('0-rotate_2d_matrix').rotate_2d_matrix
+
+if __name__ == "__main__":
+    matrix = [[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]]
+
+    rotate_2d_matrix(matrix)
+    print(matrix)
+
+jessevhedden$
+jessevhedden$ ./main_0.py
+[[7, 4, 1],
+[8, 5, 2],
+[9, 6, 3]]
